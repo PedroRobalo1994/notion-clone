64152d4692ff2dd2b3a84830a0041117305a6332
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2023-10-17 23:43:30 +0100

    Authentication and theme now working issue with https on the domain url

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://caring-walleye-14.clerk.accounts.dev",
+      domain: "caring-walleye-14.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
